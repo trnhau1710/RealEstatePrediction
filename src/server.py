f161362a22a6c36a79e15cbf4a0793ffd545746f
trnhau1710 <2251050028hau@ou.edu.vn>
2025-09-05 15:25:30 +0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -57,3 +57,6 @@ def predict(data: RealEstateInput):
     except Exception as e:
         return {"error": str(e)}
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("src.server:app", host="0.0.0.0", port=8000)
